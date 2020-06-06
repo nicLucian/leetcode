30870b2b89eeaaebf67d75a9cac1e9981a1703c9
lucian <543140360@qq.com>
2020-06-06 23:29:22 +0800

    solve problem 704

---
+++ src/problem704/Solution.java
@@ -0,0 +1,22 @@
+package problem704;
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int index = -1;
+        int left = 0;
+        int right = nums.length - 1;
+        int mid = 0;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                index = mid;
+                break;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        return index;
+    }
+}
