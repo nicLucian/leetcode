43e696aec05adbbc3bde11a675fa215d62c2c143
lucian <543140360@qq.com>
2020-12-01 22:50:25 +0800

    solve problem 94

---
+++ src/problem94/Solution.java
@@ -0,0 +1,30 @@
+package problem94;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    private final List<Integer> mNodes = new ArrayList<>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        inorder(root);
+        return mNodes;
+    }
+
+    private void inorder(TreeNode treeNode) {
+        if (treeNode != null) {
+            inorder(treeNode.left);
+            mNodes.add(treeNode.val);
+            inorder(treeNode.right);
+        }
+    }
+}
