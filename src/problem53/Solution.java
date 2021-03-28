6ea259ceeb4dc8104906b0ea17a70e4bea93bfc3
lucian <543140360@qq.com>
2021-03-28 13:35:35 +0800

    update problem 53

--- src/problem53/Solution.java
+++ src/problem53/Solution.java
@@ -3,29 +3,27 @@ package problem53;
 import java.util.Arrays;
 
 /**
- * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * 给定一个整数数组 nums，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
  *
  * 示例:
  *
  * 输入: [-2,1,-3,4,-1,2,1,-5,4],
  * 输出: 6
- * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ * 解释:连续子数组[4,-1,2,1] 的和最大，为6。
  *
  */
 class Solution {
     public int maxSubArray(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
-        int maxSum = dp[0];
+        int result = dp[0];
         for (int i = 1; i < nums.length; i++) {
             dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
-            if (dp[i] > maxSum) {
-                maxSum = dp[i];
+            if (dp[i] > result) {
+                result = dp[i];
             }
         }
-        return maxSum;
+        System.out.println(Arrays.toString(dp));
+        return result;
     }
 }
