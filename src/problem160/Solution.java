5c0d509e6c9f9991c6a4e144c852879f2cc4861d
lucian <543140360@qq.com>
2021-03-28 13:22:21 +0800

    solve problem 160

---
+++ src/problem160/Solution.java
@@ -0,0 +1,16 @@
+package problem160;
+
+import util.ListNode;
+
+class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode A = headA;
+        ListNode B = headB;
+        while (A != B) {
+            A = (A == null) ? headB : A.next;
+            B = (B == null) ? headA : B.next;
+        }
+
+        return A;
+    }
+}
